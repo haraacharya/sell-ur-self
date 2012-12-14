57aafc3684563e99582daa0cad8341e2cf928668
haraacharya <hara.acharya@gmail.com>
2012-12-14 15:12:53 -0800

    with rvm version control manager

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,5 @@ class PostsController < ApplicationController
 
 	def index
 		@posts = Post.all
-
-		respond_to do |format|
-			format.html #index.html.erb
-			format.json {render :json => @posts}
-		end
 	end	
 end	
